58a84c79eee1428b43a420c49c5e2fe6c104577f
Tim Honchel <tim.honchel@gmail.com>
2022-01-17 12:49:47 -0500

    first commit

---
+++ StudentRewardsStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StudentRewardsStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
